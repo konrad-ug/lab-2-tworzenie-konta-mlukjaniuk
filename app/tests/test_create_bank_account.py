851dea62e83a690d1b7d9daa940a97eab63f2125
Marcelina Łukjaniuk <ma.lukjaniuk@gmail.com>
2022-10-25 22:53:13 +0200

    feature 2 PESEL

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -6,7 +6,11 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "12345678901")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+
+    def test_pesel(self):
+        konto = Konto("Dariusz", "Januszewski", "12345678901")
+        self.assertEqual(konto.pesel, "12345678901", "Pesel nie został zapisany!")
