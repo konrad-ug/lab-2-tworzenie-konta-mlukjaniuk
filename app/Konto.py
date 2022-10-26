70efd86e068e60f27f539707a5d6e89d07d003e0
Marcelina Łukjaniuk <ma.lukjaniuk@gmail.com>
2022-10-26 17:01:30 +0200

    feature 5 kod rabatowy po 1960

--- app/Konto.py
+++ app/Konto.py
@@ -8,13 +8,19 @@ class Konto:
         self.saldo = 0
         self.pesel = pesel
         self.czyPeselJestPoprawny()
-        if kod_rabatowy is not None:
+        if kod_rabatowy is not None and self.czyKlientPo1960() is True:
             self.realizacjaKoduRabatowego(kod_rabatowy)
-
 
     def czyPeselJestPoprawny(self):
         if len(self.pesel) != 11:
             self.pesel = "Niepoprawny pesel!"
+
+    def czyKlientPo1960(self):
+        rok_urodzenia = int(self.pesel[0:2])
+        if rok_urodzenia <= 60:
+            return False
+        else:
+            return True
 
     def realizacjaKoduRabatowego(self, kod_rabatowy):
         if re.match(r"^PROM_[a-zA-Z0-9][a-zA-Z0-9]\b", kod_rabatowy):
