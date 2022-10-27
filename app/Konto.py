dd5d163bc39512bc19a49e84e3ba4fbfd74e8f11
Marcelina Łukjaniuk <ma.lukjaniuk@gmail.com>
2022-10-27 14:38:28 +0200

    feature 5 fix

--- app/Konto.py
+++ app/Konto.py
@@ -8,19 +8,27 @@ class Konto:
         self.saldo = 0
         self.pesel = pesel
         self.czyPeselJestPoprawny()
-        if kod_rabatowy is not None and self.czyKlientPo1960() is True:
+        if kod_rabatowy is not None and self.czyKlientPo1960():
             self.realizacjaKoduRabatowego(kod_rabatowy)
 
     def czyPeselJestPoprawny(self):
         if len(self.pesel) != 11:
             self.pesel = "Niepoprawny pesel!"
 
-    def czyKlientPo1960(self):
-        rok_urodzenia = int(self.pesel[0:2])
-        if rok_urodzenia <= 60:
-            return False
+    def rokUrodzeniaKlienta(self):
+        cyfra_kontrolna = int(self.pesel[2])
+        if cyfra_kontrolna == 0 or cyfra_kontrolna == 1:
+            rok_urodzenia = 1900 + int(self.pesel[0:2])
         else:
+            rok_urodzenia = 2000 + int(self.pesel[0:2])
+        return rok_urodzenia
+
+    def czyKlientPo1960(self):
+        if self.rokUrodzeniaKlienta() > 1960:
             return True
+        else:
+            return False
+
 
     def realizacjaKoduRabatowego(self, kod_rabatowy):
         if re.match(r"^PROM_[a-zA-Z0-9][a-zA-Z0-9]\b", kod_rabatowy):
